a687556c50f0d95bbabbefdf58465c9e856f3c86
Enretro <envpogled@gmail.com>
2021-11-21 09:28:05 +0100

    edit variables

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,11 +1,12 @@
 const items = require('../assets/data')
 
-
 exports.handler = async (event, contex, cb) => {
   console.log(event);
   return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     statusCode: 200,
-    // body: JSON.stringify(person)
     body: JSON.stringify(items)
   }
 }
